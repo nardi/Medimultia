f84300835713d3eb99b6575e783afd955a5ee67b
MetalPinguin <gitpinguin@gmail.com>
2013-06-10 17:12:27 +0200

    Bilinear jwz

--- TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
+++ TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
@@ -9,28 +9,54 @@ import android.graphics.PointF;
 
 public class Rotator {
 	public static void rotate(int[] dest, int[] src, int width, int height, double angle) {
+		//TODO Deze 1 keer laten alloceren.
 		PointF center = new PointF(width/2f, height/2f);
 		Point destPixel = new Point();
 		PointF destPoint = new PointF();
 		PointF srcPoint = new PointF();
 		Point srcPixel = new Point();
 		
+		Point lowerLeft = new Point();
+		Point lowerRight = new Point();
+		Point upperLeft = new Point();
+		Point upperRight = new Point();
+		
+		double lowerColour;
+		int upperColour;
+		int colour;
+		
 		float cos = (float)Math.cos(angle), sin = (float)Math.sin(angle);
 		
 		for (destPixel.x = 0; destPixel.x < width; destPixel.x++) {
 			for (destPixel.y = 0; destPixel.y < height; destPixel.y++) {
+				int destPixelLocation = destPixel.x + destPixel.y * width;
+
 				destPoint.x = destPixel.x - center.x;
 				destPoint.y = destPixel.y - center.y;
 				
 				srcPoint.x = destPoint.x * cos - destPoint.y * sin;
 				srcPoint.y = destPoint.x * sin + destPoint.y * cos;
 
-				// subPixel interpoleren naar srcPixel
+				/*subPixel interpoleren naar srcPixel (Nearest neighbour) */
 				srcPixel.x = Math.round(srcPoint.x + center.x);
 				srcPixel.y = Math.round(srcPoint.y + center.y);
-
-				int destPixelLocation = destPixel.x + destPixel.y * width;
-
+				
+				/*Bilinear interpolation */
+				lowerLeft.x = (int) srcPoint.x;
+				lowerLeft.y = (int)	srcPoint.y;
+				
+				upperLeft.x = (int) srcPoint.x;
+				upperLeft.y = (int) srcPoint.y + 1;
+				
+				lowerRight.x = (int) srcPoint.x + 1;
+				lowerRight.y = (int) srcPoint.y;
+				
+				upperRight.x = (int)srcPoint.x + 1;
+				upperRight.y = (int)srcPoint.y + 1;		
+				
+				lowerColour = ((lowerRight.x - srcPoint.x) * src[lowerLeft.x + lowerLeft.y * width]) + ((srcPoint.x - lowerLeft.x) * src[lowerRight.x + lowerRight.y * width]);
+				// Niet meer Bilinear interpolation
+				
 				if (srcPixel.x < 0 || srcPixel.x >= width
 				 || srcPixel.y < 0 || srcPixel.y >= height) {
 					dest[destPixelLocation] = 0;
