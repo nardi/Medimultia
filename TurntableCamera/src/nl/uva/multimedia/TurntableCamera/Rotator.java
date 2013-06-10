380e976979a7089b630254acd8937bfa55381c52
Nardi Lam <me@nardilam.nl>
2013-06-10 13:56:00 +0200

    draaien yeah

--- TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
+++ TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
@@ -8,7 +8,7 @@ import android.graphics.Point;
 import android.graphics.PointF;
 
 public class Rotator {
-	public static void rotate(int[] dest, int[] src, int width, int height, float angle) {
+	public static void rotate(int[] dest, int[] src, int width, int height, double angle) {
 		PointF center = new PointF(width/2f, height/2f);
 		Point destPixel = new Point();
 		PointF destPoint = new PointF();
@@ -21,21 +21,22 @@ public class Rotator {
 			for (destPixel.y = 0; destPixel.y < height; destPixel.y++) {
 				destPoint.x = destPixel.x - center.x;
 				destPoint.y = destPixel.y - center.y;
-
+				
 				srcPoint.x = destPoint.x * cos - destPoint.y * sin;
 				srcPoint.y = destPoint.x * sin + destPoint.y * cos;
 
 				// subPixel interpoleren naar srcPixel
-				srcPixel.x = Math.round(srcPoint.x);
-				srcPixel.y = Math.round(srcPoint.y);
+				srcPixel.x = Math.round(srcPoint.x + center.x);
+				srcPixel.y = Math.round(srcPoint.y + center.y);
 
-				int srcPixelLocation = srcPixel.x + srcPixel.y * width;
 				int destPixelLocation = destPixel.x + destPixel.y * width;
-
-				if (srcPixelLocation < src.length)
-					dest[destPixelLocation] = src[srcPixelLocation];
-				else
+				if (srcPixel.x < 0 || srcPixel.x >= width
+				 || srcPixel.y < 0 || srcPixel.y >= height) {
 					dest[destPixelLocation] = 0;
+				} else {
+					int srcPixelLocation = srcPixel.x + srcPixel.y * width;
+					dest[destPixelLocation] = src[srcPixelLocation];
+				}					
 			}
 		}
 	}
