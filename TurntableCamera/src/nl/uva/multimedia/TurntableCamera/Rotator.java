9d61d6c895df489c88b02bb7c7acfe3fd4c07e75
Nardi Lam <me@nardilam.nl>
2013-06-10 13:29:02 +0200

    Draaien misschien?

--- TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
+++ TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
@@ -1,9 +1,42 @@
 package nl.uva.multimedia.TurntableCamera;
 
+/*
+ * Rotators gonna rotate
+ */
+
 import android.graphics.Point;
+import android.graphics.PointF;
 
 public class Rotator {
-	public static void rotate(int[] dest, int[] src, Point size, float angle) {
-		// Draai het ding
+	public static void rotate(int[] dest, int[] src, int width, int height, float angle) {
+		PointF center = new PointF(width/2f, height/2f);
+		Point destPixel = new Point();
+		PointF destPoint = new PointF();
+		PointF srcPoint = new PointF();
+		Point srcPixel = new Point();
+		
+		float cos = (float)Math.cos(angle), sin = (float)Math.sin(angle);
+		
+		for (destPixel.x = 0; destPixel.x < width; destPixel.x++) {
+			for (destPixel.y = 0; destPixel.y < height; destPixel.y++) {
+				destPoint.x = destPixel.x - center.x;
+				destPoint.y = destPixel.y - center.y;
+
+				srcPoint.x = destPoint.x * cos - destPoint.y * sin;
+				srcPoint.y = destPoint.x * sin + destPoint.y * cos;
+
+				// subPixel interpoleren naar srcPixel
+				srcPixel.x = Math.round(srcPoint.x);
+				srcPixel.y = Math.round(srcPoint.y);
+
+				int srcPixelLocation = srcPixel.x + srcPixel.y * width;
+				int destPixelLocation = destPixel.x + destPixel.y * width;
+
+				if (srcPixelLocation < src.length)
+					dest[destPixelLocation] = src[srcPixelLocation];
+				else
+					dest[destPixelLocation] = 0;
+			}
+		}
 	}
 }
