3d23451b33f604fb8403ebe2870d7db21ebf3313
Nardi Lam <me@nardilam.nl>
2013-06-10 12:24:06 +0200

    Ding

--- TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
+++ TurntableCamera/src/nl/uva/multimedia/TurntableCamera/Rotator.java
@@ -3,7 +3,7 @@ package nl.uva.multimedia.TurntableCamera;
 import android.graphics.Point;
 
 public class Rotator {
-	public static void rotate(int[] dest, int[] src, Point size, float arc) {
+	public static void rotate(int[] dest, int[] src, Point size, float angle) {
 		// Draai het ding
 	}
 }
