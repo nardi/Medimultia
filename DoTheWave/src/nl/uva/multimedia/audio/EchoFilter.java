ac1b226c3e4573fb0509b389ad2999562dccf9fb
Nardi Lam <me@nardilam.nl>
2013-06-12 13:15:38 +0200

    Soort van een echo ding

--- DoTheWave/src/nl/uva/multimedia/audio/EchoFilter.java
+++ DoTheWave/src/nl/uva/multimedia/audio/EchoFilter.java
@@ -12,16 +12,25 @@ package nl.uva.multimedia.audio;
 /* XXX Yes, you should change stuff here */
 
 public class EchoFilter {
-
-	
-	public EchoFilter()
-	{
-		// XXX Add your code here
-	}
+	int delayLength = 40000;
+	double feedback = 0.4;
+	CircularBuffer buffer = new CircularBuffer(delayLength);
+	int inPos, outPos, samplesPassed;
+	short[] echo;
 	
 	public void filter(short[] samples, int length)
 	{
-		// XXX Add your code here
+		if (echo == null || length != echo.length) {
+			echo = new short[length];
+		}
+		
+		inPos = buffer.placeFrom(samples, inPos);
+		samplesPassed += length;
+		
+		if (samplesPassed >= delayLength) {
+			outPos = buffer.getFrom(echo, outPos);
+			for (int i = 0; i < length; i++)
+				samples[i] += (short)(echo[i] * feedback);
+		}
 	}
-	
 }
