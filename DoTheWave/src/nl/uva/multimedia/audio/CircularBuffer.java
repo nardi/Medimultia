b87a0f64da7e76bd093d1a8bf2b02148d0edcb30
Nardi Lam <me@nardilam.nl>
2013-06-12 13:16:31 +0200

    Soort van een echo ding

---
+++ DoTheWave/src/nl/uva/multimedia/audio/CircularBuffer.java
@@ -0,0 +1,33 @@
+package nl.uva.multimedia.audio;
+
+public class CircularBuffer {
+	private short[] buffer;
+	
+	public CircularBuffer(int size) {
+		buffer = new short[size];
+	}
+
+	public int getFrom(short[] data, int position) {
+		int bufferLeft = buffer.length - position;
+		if (bufferLeft >= data.length) {
+			System.arraycopy(buffer, position, data, 0, data.length);
+		} else {
+			System.arraycopy(buffer, position, data, 0, bufferLeft);
+			System.arraycopy(buffer, 0, data, bufferLeft, data.length - bufferLeft);
+		}
+		
+		return (position + data.length) % buffer.length;
+	}
+
+	public int placeFrom(short[] data, int position) {
+		int bufferLeft = buffer.length - position;
+		if (bufferLeft >= data.length) {
+			System.arraycopy(data, 0, buffer, position, data.length);
+		} else {
+			System.arraycopy(data, 0, buffer, position, bufferLeft);
+			System.arraycopy(data, bufferLeft, buffer, 0, data.length - bufferLeft);
+		}
+		
+		 return (position + data.length) % buffer.length;
+	}
+}
