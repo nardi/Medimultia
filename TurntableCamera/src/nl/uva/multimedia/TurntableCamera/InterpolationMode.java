a24b8f10cf969b193ea19e85ce7de0d98364651f
Nardi Lam <me@nardilam.nl>
2013-06-12 11:33:28 +0200

    Commentaar

--- TurntableCamera/src/nl/uva/multimedia/TurntableCamera/InterpolationMode.java
+++ TurntableCamera/src/nl/uva/multimedia/TurntableCamera/InterpolationMode.java
@@ -1,5 +1,9 @@
 package nl.uva.multimedia.TurntableCamera;
 
+/*
+ * This is an enum containing the different types of pixel interpolation.
+ */
+
 public enum InterpolationMode {
 	NEAREST_NEIGHBOR,
 	BILINEAR
