45252d6019e4d414aa20e6517842a4da1ae16a49
Nardi Lam <me@nardilam.nl>
2013-06-12 14:37:42 +0200

    Echo beter denk ik

--- DoTheWave/src/nl/uva/multimedia/audio/EchoFilter.java
+++ DoTheWave/src/nl/uva/multimedia/audio/EchoFilter.java
@@ -9,29 +9,32 @@ package nl.uva.multimedia.audio;
  * I.M.J. Kamps, S.J.R. van Schaik, R. de Vries (2013)
  */
 
-/* XXX Yes, you should change stuff here */
-
 public class EchoFilter {
-	int delayLength = 44100;
-	double feedback = 0.4;
-	CircularBuffer buffer = new CircularBuffer(delayLength);
-	int inPos, outPos, samplesPassed;
+	private int delayLength = 44100;
+	double feedback = 0.5;
+	CircularBuffer buffer;
+	int position;
 	short[] echo;
 	
 	public void filter(short[] samples, int length)
 	{
 		if (echo == null || length != echo.length) {
 			echo = new short[length];
+			buffer = new CircularBuffer(delayLength + length);
 		}
 
-		samplesPassed += length;
+		buffer.getFrom(position + delayLength, echo, length);
+		for (int i = 0; i < length; i++)
+			samples[i] += (short)(echo[i] * feedback);
 		
-		if (samplesPassed >= delayLength) {
-			outPos = buffer.getFrom(outPos, echo, length);
-			for (int i = 0; i < length; i++)
-				samples[i] += (short)(echo[i] * feedback);
-		}
-		
-		inPos = buffer.placeFrom(inPos, samples, length);
+		position = buffer.placeFrom(position, samples, length);
+	}
+	
+	public void setDelay(double seconds) {
+		delayLength = (int)(waveFile.getSampleRate() * seconds);
+	}
+	
+	public void setFeedback(double factor) {
+		feedback = factor;		
 	}
 }
