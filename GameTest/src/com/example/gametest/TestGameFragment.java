11a94b1baa57f0583ce22e66d4261eb7dd4fdd2b
Nardi Lam <me@nardilam.nl>
2013-06-12 15:17:56 +0200

    Cool ding

--- GameTest/src/com/example/gametest/TestGameFragment.java
+++ GameTest/src/com/example/gametest/TestGameFragment.java
@@ -12,14 +12,21 @@ public class TestGameFragment extends GameFragment {
 
 	long totalTime;
 	
+	/*
+	 * Update the game state: dt is the elapsed time in milliseconds since
+	 * the (start of the) last update.
+	 */
 	@Override
 	public void onUpdate(long dt) {
 		Log.d("TestGameFragment", "onUpdate: dt = " + dt);
 		totalTime += dt;
 		Log.d("TestGameFragment", "onUpdate: totalTime = " + totalTime);
-		SystemClock.sleep(10);
+		SystemClock.sleep(5);
 	}
 
+	/*
+	 * Draw the game: only on this canvas! (Threads enzo)
+	 */
 	@Override
 	public void onDraw(Canvas canvas) {
 		canvas.drawRGB(100, 149, 237);
