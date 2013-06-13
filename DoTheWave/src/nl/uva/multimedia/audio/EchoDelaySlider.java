300174db8981d729835e32d998972fe600525144
MetalPinguin <gitpinguin@gmail.com>
2013-06-13 02:20:33 +0200

    files

---
+++ DoTheWave/src/nl/uva/multimedia/audio/EchoDelaySlider.java
@@ -0,0 +1,42 @@
+package nl.uva.multimedia.audio;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.SeekBar;
+
+public class EchoDelaySlider extends MySlider {
+	
+	public EchoFilter echofilter;
+
+	public EchoDelaySlider(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EchoDelaySlider(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public EchoDelaySlider(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void onProgressChanged(SeekBar slider, int progress,
+			boolean from_user) {
+
+		/* Do something with progress here */
+		Log.e("Slider","Progress is set to: " + progress);
+		float delay = progress/100 * 2;
+		if(echofilter != null){
+			echofilter.setDelay(delay);
+		}
+	}
+	
+	public void setEchoFilter(EchoFilter echofilter){
+		this.echofilter = echofilter;
+	}
+
+}
