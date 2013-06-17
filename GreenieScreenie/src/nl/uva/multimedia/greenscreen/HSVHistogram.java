d8475ae0499138a6a2541e87dc1ffe759488dc93
Nardi Lam <me@nardilam.nl>
2013-06-17 15:25:57 +0200

    beterrr

--- GreenieScreenie/src/nl/uva/multimedia/greenscreen/HSVHistogram.java
+++ GreenieScreenie/src/nl/uva/multimedia/greenscreen/HSVHistogram.java
@@ -10,15 +10,8 @@ public class HSVHistogram extends Histogram {
 	public HSVHistogram(Point pos, Point size) {
 		super(pos, size, new PointF(360, 1));
 	}
-
-	private int[] pixelBins;
+	
 	private float[] hsv = new float[3];
-	
-	public void setSize(Point size) {
-		super.setSize(size);
-		pixelBins = new int[size.x * size.y];
-	}
-	
 	private Paint paint = new Paint();
 
 	/*
@@ -26,39 +19,15 @@ public class HSVHistogram extends Histogram {
 	 * 
 	 * TODO Deze methode (en klasse) kan gedeeltelijk naar Histogram verplaatst worden denk ik
 	 */
-	public void draw(Canvas canvas, float[] data) {
-		if (data.length == 0)
-			return;
+	protected void drawPixel(Canvas canvas, int x, int y, int amount, int maxAmount) {
+		hsv[0] = x * size.x / range.x;
+		hsv[1] = y * size.y / range.y;
+		hsv[2] = (float)amount / maxAmount;
 
-		for (int i = 0; i < pixelBins.length; i++) {
-			pixelBins[i] = 0;
-		}
-		
-		int maxAmount = 1;
-		for (int i = 0; i < data.length; i += 2) {
-			int x = (int)(size.x * data[i] / range.x);
-			int y = (int)(size.y * data[i + 1] / range.y);
-			
-			int amount = ++pixelBins[x + y * size.x];
-			if (amount > maxAmount) {
-				maxAmount = amount;
-			}
-		}
-		
-		canvas.save();
-		canvas.translate(position.x, position.y);
-		
-		for (int x = 0; x < size.x; x++) {
-			for (int y = 0; y < size.y; y++) {
-				hsv[0] = x * size.x / range.x;
-				hsv[1] = y * size.y / range.y;
-				hsv[2] = (float)pixelBins[x + y * size.x] / maxAmount;
-
-				paint.setColor(Color.HSVToColor(hsv));
-				canvas.drawPoint(x, y, paint);
-			}
-		}
-
-		canvas.restore();
+		if (amount == 0)
+			paint.setColor(0);
+		else
+			paint.setColor(Color.HSVToColor(hsv));
+		canvas.drawPoint(x, y, paint);
 	}
 }
--- GreenieScreenie/src/nl/uva/multimedia/greenscreen/Histogram.java
+++ GreenieScreenie/src/nl/uva/multimedia/greenscreen/Histogram.java
@@ -3,9 +3,8 @@ package nl.uva.multimedia.greenscreen;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.PointF;
-import android.graphics.RectF;
 
-public class Histogram {
+public abstract class Histogram {
 	public Point position;
 	protected Point size;
 	public PointF range;
@@ -16,7 +15,74 @@ public class Histogram {
 		this.range = range;
 	}
 	
+	private int[] pixelBins;
+	
 	public void setSize(Point size) {
 		this.size = size;
-	}	
+		pixelBins = new int[size.x * size.y];
+	}
+	
+	protected abstract void drawPixel(Canvas canvas, int x, int y, int amount, int maxAmount);
+	
+	public void draw(Canvas canvas, float[] data) {
+		if (data.length == 0)
+			return;
+
+		for (int i = 0; i < pixelBins.length; i++) {
+			pixelBins[i] = 0;
+		}
+		
+		int maxAmount = 1;
+		for (int i = 0; i < data.length; i += 2) {
+			int x = (int)(size.x * data[i] / range.x);
+			int y = (int)(size.y * data[i + 1] / range.y);
+			
+			int amount = ++pixelBins[x + y * size.x];
+			if (amount > maxAmount) {
+				maxAmount = amount;
+			}
+		}
+		
+		canvas.save();
+		canvas.translate(position.x, position.y);
+		
+		for (int x = 0; x < size.x; x++) {
+			for (int y = 0; y < size.y; y++) {
+				drawPixel(canvas, x, y, pixelBins[x + y * size.x], maxAmount);
+			}
+		}
+
+		canvas.restore();
+	}
+	
+	public void draw(Canvas canvas, int[] data) {
+		if (data.length == 0)
+			return;
+
+		for (int i = 0; i < pixelBins.length; i++) {
+			pixelBins[i] = 0;
+		}
+		
+		int maxAmount = 1;
+		for (int i = 0; i < data.length; i += 2) {
+			int x = (int)(size.x * data[i] / range.x);
+			int y = (int)(size.y * data[i + 1] / range.y);
+			
+			int amount = ++pixelBins[x + y * size.x];
+			if (amount > maxAmount) {
+				maxAmount = amount;
+			}
+		}
+		
+		canvas.save();
+		canvas.translate(position.x, position.y);
+		
+		for (int x = 0; x < size.x; x++) {
+			for (int y = 0; y < size.y; y++) {
+				drawPixel(canvas, x, y, pixelBins[x + y * size.x], maxAmount);
+			}
+		}
+
+		canvas.restore();
+	}
 }
