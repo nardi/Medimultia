1e77345c8b8e0b3ed6bc455c28969e83f07c6718
MetalPinguin <gitpinguin@gmail.com>
2013-06-17 14:47:28 +0200

    Filetje geadd

---
+++ GreenieScreenie/src/nl/uva/multimedia/greenscreen/SwitchColourSpace.java
@@ -0,0 +1,34 @@
+package nl.uva.multimedia.greenscreen;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+
+public class SwitchColourSpace extends Switch implements
+		OnCheckedChangeListener {
+	
+	public CanvasView canvas;
+	
+	public SwitchColourSpace(Context context) {
+		super(context);
+	}
+
+	public SwitchColourSpace(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public SwitchColourSpace(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	{
+		setOnCheckedChangeListener(this);
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+		canvas.yuv = arg1;
+	}
+
+}
