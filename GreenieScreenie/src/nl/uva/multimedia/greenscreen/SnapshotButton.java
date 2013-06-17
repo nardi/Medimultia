afd99fa124745f3b9172e70c297a27d27677c187
MetalPinguin <gitpinguin@gmail.com>
2013-06-17 17:08:06 +0200

    Shit vergeten te adden

---
+++ GreenieScreenie/src/nl/uva/multimedia/greenscreen/SnapshotButton.java
@@ -0,0 +1,35 @@
+package nl.uva.multimedia.greenscreen;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.view.View;
+
+public class SnapshotButton extends Button implements View.OnClickListener {
+	
+	public CanvasView canvas;
+		
+	public SnapshotButton(Context context) {
+		super(context);
+	}
+
+	public SnapshotButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public SnapshotButton(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	
+	{
+		setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		canvas.snapshot();		
+	}
+
+}
