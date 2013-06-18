1a7e9a187e4f1ecb1435969a34707237c85b9f26
Nardi Lam <me@nardilam.nl>
2013-06-18 15:54:51 +0200

    perfecte green screen

---
+++ GreenieScreenie/src/nl/uva/multimedia/greenscreen/GreenScreenSwitch.java
@@ -0,0 +1,35 @@
+package nl.uva.multimedia.greenscreen;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+
+public class GreenScreenSwitch extends Switch implements
+		OnCheckedChangeListener {
+	
+	public CanvasView canvas;
+	
+	public GreenScreenSwitch(Context context) {
+		super(context);
+	}
+
+	public GreenScreenSwitch(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public GreenScreenSwitch(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+	
+	{
+		setOnCheckedChangeListener(this);
+	}
+
+	@Override
+	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+		canvas.doGreenScreen = arg1;
+	}
+
+}
