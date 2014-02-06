1a6e82793767737eb007b2d66e11a43d44d52c43
MetalPinguinInc <10439013@u030215.fnwi.uva.nl>
2014-02-06 14:39:05 +0100

    Triangles bitch

--- gfx/framework1/triangle_rasterization/trirast.c
+++ gfx/framework1/triangle_rasterization/trirast.c
@@ -26,9 +26,39 @@
  * The triangle is drawn in color (r,g,b).
  */
 
+float f20(float x, float y, float x0, float y0, float x2, float y2){
+	return (y2 - y0) * x + (x0 - x2) * y + x2 * y0 - x0 * y2;
+}
+
+float f01(float x, float y, float x1, float y1, float x0, float y0){
+	return (y0 - y1) * x + (x1 - x0) * y + x0 * y1 - x1 * y0;
+}
+
 void
 draw_triangle(float x0, float y0, float x1, float y1, float x2, float y2,
     byte r, byte g, byte b) {
+		int ymin = (int)fmin(fmin(y0, y1), y2);
+		int ymax = (int)fmax(fmax(y0, y1), y2);
+
+		int xmin = (int)fmin(fmin(x0,x1),x2);
+		int xmax = (int)fmax(fmax(x0, x1),x2);
+		
+		float beta, alpha, gamma;
+
+		for(int y = ymin; y <= ymax; y++){
+			for(int x = xmin; x <= xmax; x++){
+				beta = f20(x, y, x0 ,y0, x2, y2) / f20(x1, y1, x0, y0, x2, y2);
+				gamma = f01(x, y, x1, y1, x0, y0) / f01(x2, y2, x1, y1, x0, y0);
+				alpha = 1 - beta - gamma;
+				if (alpha > 0 && beta > 0 && gamma > 0){
+				// dan ook < 1
+					
+					//PutPixel(x, y, alpha * r, beta * g, gamma *b);
+					PutPixel(x, y, r, g, b);
+				}
+			}		
+		}
+
     
 }
 
