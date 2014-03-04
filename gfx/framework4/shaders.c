1daa2ac2a02487a7ca8d0a33be51b5964c489805
Nardi Lam <me@nardilam.nl>
2014-03-04 15:17:58 +0100

    Alle shaders :3

--- gfx/framework4/shaders.c
+++ gfx/framework4/shaders.c
@@ -36,19 +36,51 @@ shade_constant(intersection_point ip)
 vec3
 shade_matte(intersection_point ip)
 {
-    return v3_create(1, 0, 0);
+    float l_here, l_full;
+    
+    l_here = l_full = scene_ambient_light;
+    
+    for (int i = 0; i < scene_num_lights; i++)
+    {
+        l_full += scene_lights[i].intensity;
+    
+        vec3 li = v3_normalize(v3_subtract(scene_lights[i].position, ip.p));
+        
+        if (!shadow_check(v3_add(ip.p, v3_multiply(li, 0.0001)), li))
+            l_here += scene_lights[i].intensity * fmax(0, v3_dotprod(ip.n, li));
+    }
+    
+    return v3_multiply(v3_create(1, 1, 1), l_here / l_full);
 }
 
 vec3
 shade_blinn_phong(intersection_point ip)
 {
-    return v3_create(1, 0, 0);
+    float kd = 0.8, ks = 0.5, a = 50, diffuse = 0, specular = 0;
+    vec3 cd = v3_create(1,0,0), cs = v3_create(1,1,1);
+    
+    for (int i = 0; i < scene_num_lights; i++)
+    {    
+        vec3 li = v3_normalize(v3_subtract(scene_lights[i].position, ip.p)),
+             h = v3_normalize(v3_add(ip.i, li));
+        
+        diffuse += scene_lights[i].intensity * fmax(0, v3_dotprod(ip.n, li));
+        specular += scene_lights[i].intensity * pow(v3_dotprod(ip.n, h), a);
+    }
+    
+    return v3_add(v3_multiply(cd, scene_ambient_light + kd * diffuse),
+                  v3_multiply(cs, ks * specular));
 }
 
 vec3
 shade_reflection(intersection_point ip)
 {
-    return v3_create(1, 0, 0);
+    vec3 matte = shade_matte(ip),
+         r = v3_subtract(v3_multiply(ip.n, 2 * v3_dotprod(ip.i, ip.n)), ip.i),
+         p_offset = v3_add(ip.p, v3_multiply(r, 0.0001)),
+         reflect = ray_color(ip.ray_level + 1, p_offset, r);
+         
+    return v3_add(v3_multiply(matte, 0.75), v3_multiply(reflect, 0.25));
 }
 
 // Returns the shaded color for the given point to shade.
